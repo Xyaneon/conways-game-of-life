0c34c2a27e850c86b79a3c0554928aee3beeb53c
Xyaneon <christhehorton@gmail.com>
2022-06-25 13:41:54 -0400

    Create Avalonia UI project
    http://avaloniaui.net/

---
+++ src/Xyaneon.Games.ConwaysGameOfLife.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Xyaneon.Games.ConwaysGameOfLife.Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
