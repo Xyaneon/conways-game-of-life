ff790310815012d997668d91b7a7f968fc0df84d
Xyaneon <christhehorton@gmail.com>
2022-06-25 18:00:51 -0400

    feat: implement quit command in menu

--- src/Xyaneon.Games.ConwaysGameOfLife.Avalonia/ViewModels/MainWindowViewModel.cs
+++ src/Xyaneon.Games.ConwaysGameOfLife.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,33 @@
-﻿namespace Xyaneon.Games.ConwaysGameOfLife.Avalonia.ViewModels
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using ReactiveUI;
+using System.Reactive;
+
+namespace Xyaneon.Games.ConwaysGameOfLife.Avalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public MainWindowViewModel() {
+            QuitCommand = ReactiveCommand.Create(RunQuitCommand);
+        }
+
         public string Greeting => "Welcome to Avalonia!";
+
+        public ReactiveCommand<Unit, Unit> QuitCommand { get; }
+
+        void RunQuitCommand()
+        {
+            Application? currentApplication = Application.Current;
+            if (currentApplication != null)
+            {
+                IApplicationLifetime? lifetime = currentApplication.ApplicationLifetime;
+                if (lifetime != null)
+                {
+                    var classicDesktopLifetime = (IClassicDesktopStyleApplicationLifetime)lifetime;
+                    classicDesktopLifetime.Shutdown();
+                }
+            }
+        }
     }
+    
 }
