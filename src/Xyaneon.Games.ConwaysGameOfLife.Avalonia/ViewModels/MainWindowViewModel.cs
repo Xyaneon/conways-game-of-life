4a5016e7477a2cbdf6f60ad321f33858127a6092
Xyaneon <christhehorton@gmail.com>
2022-06-26 00:15:43 -0400

    feat(ui): read the selected file, keep name and description

--- src/Xyaneon.Games.ConwaysGameOfLife.Avalonia/ViewModels/MainWindowViewModel.cs
+++ src/Xyaneon.Games.ConwaysGameOfLife.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using ReactiveUI;
 using System.Reactive;
 using System.Threading.Tasks;
+using Xyaneon.Games.ConwaysGameOfLife.FileIO.Plaintext;
 
 namespace Xyaneon.Games.ConwaysGameOfLife.Avalonia.ViewModels;
 
@@ -14,9 +15,25 @@ public class MainWindowViewModel : ViewModelBase
         QuitCommand = ReactiveCommand.Create(RunQuitCommand);
     }
 
+    private string? _patternDescription;
+    private string? _patternName;
+
     public string Greeting => "Welcome to Avalonia!";
 
     public ReactiveCommand<Unit, Unit> OpenCommand { get; }
+
+    public string? PatternDescription
+    {
+        get => _patternDescription;
+        set => this.RaiseAndSetIfChanged(ref _patternDescription, value);
+    }
+    
+    public string? PatternName
+    {
+        get => _patternName;
+        set => this.RaiseAndSetIfChanged(ref _patternName, value);
+    }
+
     public ReactiveCommand<Unit, Unit> QuitCommand { get; }
 
     public async Task RunOpenCommand()
@@ -38,6 +55,10 @@ public class MainWindowViewModel : ViewModelBase
                 foreach (var path in result)
                 {
                     System.Console.WriteLine($"path: {path}");
+                    var fileInfo = new System.IO.FileInfo(path);
+                    PlaintextFileContents fileContents = PlaintextFileReader.ReadFile(fileInfo);
+                    PatternName = fileContents.Name;
+                    PatternDescription = fileContents.Description;
                 }
             }
         }
